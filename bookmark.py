7c68c190fc69302bc7706221bb597041c9fd3767
Nidhi Kumari <nidhi1691996@outlook.com>
2018-07-13 21:39:24 +0530

    add home.html

--- bookmark.py
+++ bookmark.py
@@ -10,11 +10,6 @@ project_dir = os.path.dirname(os.path.abspath(__file__))
 database_file = "sqlite:///{}".format(os.path.join(project_dir, "bookdatabase.db"))
 
 
-
-
-
-
-
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = database_file
 
